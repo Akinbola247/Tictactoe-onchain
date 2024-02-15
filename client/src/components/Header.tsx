6f9d291d49797852afa5c303b6b9a01bc31b496a
Faith M. Roberts <faithroberts708@gmail.com>
2024-02-15 12:35:26 +0100

    merged

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,6 +6,8 @@ import { IoRocketSharp } from "react-icons/io5";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { useDojo } from "../DojoContext";
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const Header = () => {
@@ -17,10 +19,19 @@ const Header = () => {
     account: { create, list, select, account, isDeploying, clear},
   } = useDojo();
 
-  const address = isDeploying ? "deploying burner" : (list()[0]?.address?.toString()?.slice(0, 9) || "0x0");
-
+  // const address = isDeploying ? "deploying burner" : (list()[0]?.address?.toString()?.slice(0, 9) || "0x0");
+  const handleDeploy = () =>{
+    // if(list()[0]?.address?.toString() != ''){
+    //   toast.error('burner already deployed')
+    // }else {
+      create()
+    toast("Burner deployed");
+    // }
+    
+  }
   return (
     <div className='w-[100%] h-[80px] absolute border border-b-[#000000]'>
+      <ToastContainer />
 
         <div className='flex flex-row justify-between items-center w-[85%] mx-auto'>
         
@@ -32,13 +43,13 @@ const Header = () => {
 
         <h1 className='press text-[12px]'>About us</h1>
         
-        <div className='flex space-x-1 bg-[#FF3D00] w-[160px] h-[40px] border border-[#000000] rounded-lg items-center p-2' onClick={create}>
+        <div className='flex space-x-1 bg-[#FF3D00] w-[160px] h-[40px] border border-[#000000] rounded-lg items-center p-2' onClick={handleDeploy}>
             <IoRocketSharp className='text-[#FFFFFF]'/>
-            <h1 className='press text-[9px] text-[#FFFFFF] w-[100%]' >Deploy burner</h1>
+            <h1 className='press text-[9px] text-[#FFFFFF] w-[100%] cursor-pointer' >Deploy burner</h1>
         </div>
      
             <div className='flex w-[140px] border border-[#FF3D00] h-[40px]  rounded-lg items-center p-2'>
-            <h1 className='press text-[10px] text-[#FF3D00] flex mx-auto'> {address}
+            <h1 className='press text-[10px] text-[#FF3D00] flex mx-auto'> {(account?.address).slice(0, 6) ?? '0x0'}
               </h1>
             </div>
             <div className='flex w-[140px] border border-[#FF3D00] h-[40px]  rounded-lg items-center p-2'>
